e85b6b1fbd93c0a3cc4f86fe15ff30c87ba8f934
Mingzhao Zhou <edwardwhite7@hotmail.com>
2024-06-23 09:54:23 +0100

    add location to my dream locations section

--- views/components/location.js
+++ views/components/location.js
@@ -1,8 +1,15 @@
 export default function renderLocation(location) {
   return `
     <li class="location-item">
-      <button>
-        <img src="${`/images/${location.image.src}`}" alt="${location.image.alt}" />
+      <button
+        hx-post="/places"
+        hx-vals='{"locationId": "${location.id}"}'
+        hx-target="#interesting-locations"
+        hx-swap="beforeend"
+      >
+        <img src="${`/images/${location.image.src}`}" alt="${
+    location.image.alt
+  }" />
         <h3>${location.title}</h3>
       </button>
     </li>
