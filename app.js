3158bc00d30a81c0653df024dcc44e38b1a3662b
Mingzhao Zhou <edwardwhite7@hotmail.com>
2024-07-12 15:04:20 +0100

    code refactor

--- app.js
+++ app.js
@@ -32,53 +32,7 @@ const INTERESTING_LOCATIONS = [];
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  const availableLocations = AVAILABLE_LOCATIONS.filter(
-    (location) => !INTERESTING_LOCATIONS.includes(location)
-  );
-  const suggestdLocations = getSuggestedLocations();
-  res.send(
-    renderLocationsPage(
-      suggestdLocations,
-      availableLocations,
-      INTERESTING_LOCATIONS
-    )
-  );
-});
-
-app.get("/suggested-locations", (req, res) => {
-  const suggestdLocations = getSuggestedLocations();
-
-  res.send(
-    suggestdLocations.map((location) => renderLocation(location)).join("")
-  );
-});
-
-app.post("/places", (req, res) => {
-  const locationId = req.body.locationId;
-  const location = AVAILABLE_LOCATIONS.find((loc) => loc.id === locationId);
-  INTERESTING_LOCATIONS.push(location);
-
-  const availableLocations = AVAILABLE_LOCATIONS.filter(
-    (location) => !INTERESTING_LOCATIONS.includes(location)
-  );
-
-  const suggestdLocations = getSuggestedLocations();
-
-  // apply out of band swap to update the available locations and suggested locations
-  res.send(`
-    ${renderLocation(location, false)}
-
-    <ul id="suggested-location" class="locations" hx-swap-oob="innerHTML">
-      ${suggestdLocations.map((location) => renderLocation(location)).join("")}
-    </ul>
-
-    <ul id="available-locations" class="locations" hx-swap-oob="true">
-      ${availableLocations.map((location) => renderLocation(location)).join("")}
-    </ul>
-  `);
-});
-
+// login
 app.get("/login-page", (req, res) => {
   res.send(loginPage());
 });
@@ -132,6 +86,54 @@ app.post("/login", (req, res) => {
 
   res.setHeader("HX-Redirect", "/");
   res.send();
+});
+
+// app
+app.get("/", (req, res) => {
+  const availableLocations = AVAILABLE_LOCATIONS.filter(
+    (location) => !INTERESTING_LOCATIONS.includes(location)
+  );
+  const suggestdLocations = getSuggestedLocations();
+  res.send(
+    renderLocationsPage(
+      suggestdLocations,
+      availableLocations,
+      INTERESTING_LOCATIONS
+    )
+  );
+});
+
+app.get("/suggested-locations", (req, res) => {
+  const suggestdLocations = getSuggestedLocations();
+
+  res.send(
+    suggestdLocations.map((location) => renderLocation(location)).join("")
+  );
+});
+
+app.post("/places", (req, res) => {
+  const locationId = req.body.locationId;
+  const location = AVAILABLE_LOCATIONS.find((loc) => loc.id === locationId);
+  INTERESTING_LOCATIONS.push(location);
+
+  const availableLocations = AVAILABLE_LOCATIONS.filter(
+    (location) => !INTERESTING_LOCATIONS.includes(location)
+  );
+
+  const suggestdLocations = getSuggestedLocations();
+
+  // apply out of band swap to update the available locations and suggested locations
+  res.send(`
+    ${renderLocation(location, false)}
+
+    <ul id="suggested-location" class="locations" hx-swap-oob="innerHTML">
+      ${suggestdLocations.map((location) => renderLocation(location)).join("")}
+    </ul>
+
+    <ul id="available-locations" class="locations" hx-swap-oob="true">
+      ${availableLocations.map((location) => renderLocation(location)).join("")}
+    </ul>
+  `);
 });
 
 app.get("/places/:id", (req, res) => {
